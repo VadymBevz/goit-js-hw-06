abfc98b43cb639dca78691eac2975f0e3bfdebf8
VadymBevz <vbevzv@gmail.com>
2023-02-27 17:02:12 +0200

    nosrc

--- src/js/task-06.js
+++
@@ -1,5 +0,0 @@
-const validationInput = document.querySelector("#validation-input");
-validationInput.addEventListener("blur", () => {
-    validationInput.classList.toggle("valid", validationInput.value.length >= 6 );
-    validationInput.classList.toggle("invalid", validationInput.value.length < 6 )
-})
